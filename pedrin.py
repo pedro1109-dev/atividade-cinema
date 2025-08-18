d7d0986ba435b3487c1dff0ca9553baf7fb0ab03
pedro1109-dev <pedro.s.aguiar@aluno.senai.br>
2025-08-18 16:01:47 -0300

    Atividade Cinema

--- pedrin.py
+++ pedrin.py
@@ -59,17 +59,73 @@ if idade <= 18:
             elif opcao_filme == '3':
                 print('Velozes Furiosos 3')
                 print('Imprimindo ingresso!')
-                break
+        elif opcao == '6':
+            print('Saindo do sistema!')
+            break
 
 
 else: 
     print('Entrada autorizada para seguintes salas: ')
     while True :
+        print('1- Sala 1')
+        print('2- Sala 2')
+        print('3- Sala 3')
         print('4- Sala 4')
         print('5- Sala 5')
         print('6- Sair')
         opcao = input('Escolha uma opção: ')
-        if opcao == '4':
+        if opcao == '1' :
+            while True:
+                if opcao == '1' :
+                    print('1-Como treinar seu dragão(+ 10 anos)')
+                    print('2-Guardiões da Galaxia(+ 10 anos)')
+                    print('3- bela e a fera (+ 10 anos)')
+                    break
+            opcao_filme = input('Escolha um filme: ')
+            if opcao_filme == '1':
+                print('1-Como treinar seu dragão(+ 10 anos)')
+                print('Imprimindo ingresso!')
+            elif opcao_filme == '2':
+                print('2-Guardiões da Galaxia(+ 10 anos)')
+                print('Imprimindo ingresso!')
+            elif opcao_filme =='3':
+                print('3- bela e a fera (+ 10 anos)')
+                print('Imprimindo ingresso!')
+        elif opcao == '2' :
+            while True:
+                if opcao== '2':
+                    print('1-Vingadores(+ 10 anos)')
+                    print('2-Sherek 5(+ 10 anos)')
+                    print('3-Gente grande(+ 10 anos)')
+                    break
+            opcao_filme = input('Escolha um filme: ')
+            if opcao_filme =='1':
+                print('1-Vingadores(+ 10 anos)')
+                print('Imprimindo ingresso!')
+            elif opcao_filme =='2':
+                print('2-Sherek 5(+ 10 anos)')
+                print('Imprimindo ingresso!')
+            elif opcao_filme =='3':
+                print('3-Gente grande(+ 10 anos)')
+                print('Imprimindo ingresso!')
+        elif opcao == '3': 
+            while True:
+                if opcao=='3':
+                    print('1-Velozes Furiosos 1')
+                    print('2-Velozes Furiosos 2')
+                    print('3-Velozes Furiosos 3')
+                    break
+            opcao_filme = input('Escolha um fime: ')
+            if opcao_filme == '1':
+                print('Velozes Furiosos 1')
+                print('Imprimindo ingresso!')
+            elif opcao_filme == '2':
+                print('Velozes Furiosos 2')
+                print('Imprimindo ingresso!')
+            elif opcao_filme == '3':
+                print('Velozes Furiosos 3')
+                print('Imprimindo ingresso!')
+        elif opcao == '4':
             while True:
                 if opcao == '4':
                     print('1-Sexta feira 13')
